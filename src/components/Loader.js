0656f1159e95a27160bca9a3fa51791d700cd2e6
gnun <gnunua@gmail.com>
2017-05-16 18:25:57 +0400

    Implemented new dynamic loader.

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,19 +1,61 @@
-import React from "react";
+import React, {Component} from 'react';
 
-const Loader = () => {
-    return (
-        <div className="vertical-center">
-            <div className="container text-center">
-                <div className="row">
-                    <div className="col-md-4">
-                        <h3 className="text-warning">
-                            Loading...
-                        </h3>
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
+const styles = {
+
+    container: {
+        position: 'fixed',
+        left: 0,
+        right: 0,
+        top: 0,
+        bottom: 0,
+        fontSize: '60px'
+    },
+    content: {
+        textAlign: 'center',
+        position: 'absolute',
+        width: '100%',
+        marginTop: '35px',
+        color: 'cornflowerblue'
+    }
 };
 
+class Loader extends Component {
+    constructor(props) {
+        super(props);
+        this.originalText = "Loading";
+        this.state = {
+            text: this.originalText
+        };
+        this.loadingTextModifier = this.loadingTextModifier.bind(this);
+    }
+
+    loadingTextModifier() {
+        let stopper = this.originalText + '...';
+        if (this.state.text === stopper) {
+            this.setState({
+                text: this.originalText
+            });
+        } else {
+            this.setState({
+                text: this.state.text + '.'
+            });
+        }
+    }
+
+    componentDidMount() {
+        setInterval(this.loadingTextModifier, 500);
+    }
+
+    render() {
+        return (
+            <div style={styles.container}>
+                <p style={styles.content}>
+                    {this.state.text}
+                </p>
+            </div>
+        );
+    }
+}
+
 export default Loader;
+
