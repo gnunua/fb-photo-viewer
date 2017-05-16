0f4c3485208b4619632fef4b5e73f2f9361c522a
gnun <gnunua@gmail.com>
2017-05-17 00:39:42 +0400

    added transform class properties supporting, created corresponding configurations

--- webpack.config.js
+++ webpack.config.js
@@ -41,7 +41,7 @@ module.exports = {
             {
                 test: /\.js$/, exclude: /node_modules/, loader: "babel-loader",
                 query: {
-                    presets: ['es2015']
+                    presets: ['es2015', 'stage-0']
                 }
             },
             {
